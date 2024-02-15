02af7a267122dbc7047adbd93ff00253e1ba728f
Zakaria <68170594+ZackInfosbd@users.noreply.github.com>
2024-02-15 21:11:28 +0100

    cqrs/events:  query handler, integrating events triggering and initial mongodb setup for read side of application

--- src/alarms/application/commands/create-alarm.command-handler.ts
+++ src/alarms/application/commands/create-alarm.command-handler.ts
@@ -1,8 +1,9 @@
-import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { CommandHandler, EventBus, ICommandHandler } from '@nestjs/cqrs';
 import { CreateAlarmCommand } from './create-alarm.command';
 import { Logger } from '@nestjs/common';
 import { AlarmRepository } from '../ports/alarm.repository';
 import { AlarmFactory } from 'src/alarms/domain/factories/alarm.factory';
+import { AlarmCreatedEvent } from 'src/alarms/domain/events/alarm-created.event';
 
 @CommandHandler(CreateAlarmCommand)
 export class CreateAlarmCommandHandler
@@ -12,6 +13,7 @@ export class CreateAlarmCommandHandler
   constructor(
     private readonly alarmRepository: AlarmRepository,
     private readonly alarmfactory: AlarmFactory,
+    private readonly eventBus: EventBus,
   ) {}
 
   async execute(command: CreateAlarmCommand) {
@@ -19,6 +21,11 @@ export class CreateAlarmCommandHandler
       `Processing CreateAlarmCommand: ${JSON.stringify(command)}`,
     );
     const alarm = this.alarmfactory.create(command.name, command.severity);
-    return this.alarmRepository.save(alarm);
+    const newAlarm = await this.alarmRepository.save(alarm);
+
+    // this is not the best way to dispatch events.
+    // Domain events should be dispatched from the aggregate root, inside the domain layer
+    // I'll refactor this.
+    return this.eventBus.publish(new AlarmCreatedEvent(newAlarm));
   }
 }
